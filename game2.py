e9366d0a4448d4de117bc58f76d9ba5482c39735
u1and0 <e01.ando60@gmail.com>
2016-04-29 13:33:30 +0900

    Explained game2

--- game2.py
+++ game2.py
@@ -1,9 +1,35 @@
+'''
+## game2.py ver1.0
+
+__UPDATE1.0__
+First commit
+
+__USAGE__
+martingaleのメイン関数から呼び出す
+###### 相場観パラメータの設定 
+	low,high=1.01,np.inf
+	mu,si=1.4,0.3
+######  
+	x=soubakan(low, high, mu, si,1)
+	game_test(float(x))
+
+__INTRODUCTION__
+soubakan(相場観):
+	自分の取引履歴から算出した、倍率いくらのチケットを買うか、倍率を返す
+game:
+	ゲームの結果をbool値で返す
+
+__ACTION__
+
+__PLAN__
+None
+'''
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import numpy as np
 from random import random
-def soubakan(low, high, mu, si, length):
-	'''正規分布に従う乱数を返す
+def soubakan(low, high, mu, si, length=1):
+	'''正規分布に従う乱数の入ったリストを返す
 	low, high: 正規分布の最小値、最大値
 	mu, si: 正規分布の中央値、標準偏差
 	length: いくつの要素のリストを返すか'''
@@ -12,29 +38,7 @@ def soubakan(low, high, mu, si, length):
 
 
 
-def game(rate):
-	'''1-1/rateの確率で勝つゲームに勝ったらTrueを返す'''
-	x=bool(random()>=1-1/rate)
+def game(ratio):
+	'''1-1/ratioの確率で勝つゲームに勝ったらTrueを返す'''
+	x=bool(random()>=1-1/ratio)
 	return x
-
-
-
-
-'''TEST2
-テストの内容
-	何回もゲームして
-	勝率を相場観に任せるwinratea=soubakan()
-	勝率が自分の相場観にあったものか確かめる
-	いつも1.4倍くらい、つまり1/1.4=0.7位に近づくはずだ
-'''
-# low,high=1.01,np.inf
-# mu,si=1.4,0.3
-# lll=[]
-# trytime=100
-# for i in range(trytime):
-# 	x=(float(soubakan(low, high, mu, si, 1)))    #倍率
-# 	ox=1-1/x    #勝率
-# 	y=game(x)    # 勝敗
-# 	lll.append(y)
-# 	print('baititsu', x, 'winrate',ox,'Did I Win?',y)
-# print('How many time did I win', lll.count(True)/trytime)
--- game2test.py
+++ game2test.py
@@ -17,10 +17,34 @@ game2をインポートして
 __PLAN__
 None
 '''
+
+
+
+
 from game2 import *
 def soubakan_unitest():
-	'''soubakan()(相場観)のユニテスト
-	偏りをもったガウシアン状のヒストグラムがプロットされる'''
+	'''
+	## soubakan_unitest ver1.0
+	
+	__UPDATE1.0__
+	First commit
+	
+	__USAGE__
+	game_soubakan_integrationtestから呼び出される
+	
+	__INTRODUCTION__
+	soubakanのユニテスト
+	
+	__ACTION__
+	相場観パラメータを設定
+	中央値1.4、最低値1.0, 最大値無限大のヒストグラムか描かれるようにsoubakan関数が動いているか
+	プロットすることでチェックを行う
+	
+	__PLAN__
+	None
+	soubakan()(相場観)のユニテスト
+	偏りをもったガウシアン状のヒストグラムがプロットされる
+	'''
 	low,high=1.01,np.inf
 	mu,si=1.4,0.3
 	x=soubakan(low, high, mu, si,10000)
@@ -28,7 +52,38 @@ def soubakan_unitest():
 	print('maximum ratio',max(x))
 	plt.hist(x);plt.show()
 
+
+
+
+
+
+
+
+
 def game_test(ratio):
+	'''
+	## game_test ver1.0
+	
+	__UPDATE1.0__
+	First commit
+	
+	__USAGE__
+	game_unitestとgame_soubakan_integrationtestから呼び出される
+	
+	__INTRODUCTION__
+	game関数の汎用テスト関数
+	
+	__ACTION__
+	trytime=10000回くらい以下の処理を行う
+	引数game関数
+	>`1<ratio<inf`
+	>>いつも2超えるくらいまで
+	何回もゲームして
+	ゲームに勝った(Trueが返ってきた)率がratioに十分近いかを判断する
+	
+	__PLAN__
+	None
+	'''
 	trytime=10000
 	lll=[]
 	for i in range(trytime):
@@ -41,34 +96,86 @@ def game_test(ratio):
 	print('Subtraction' , abs(w-result))
 
 
+
+
+
+
+
+
+
+
 def game_unitest():
-	'''game()のユニテスト
+	'''
+	## game_unitest ver1.0
+	
+	__UPDATE1.0__
+	First commit
+	
+	__USAGE__
+	game_unitest()のコメントアウトをはずして実行
+	
+	__INTRODUCTION__
+	game()のユニテスト
+	
+	__ACTION__
 	倍率ratioを適当に決める
-	>`1<ratio<inf`
-	>>いつも2超えるくらいまで
-	何回もゲームして
-	ゲームに勝った(Trueが返ってきた)率がratioに十分近いかを判断する
+	汎用テスト関数game_test()を実行する
+	
+	__PLAN__
+	None
 	'''
 	for ratio in [1+x*0.2 for x in range(20)]:
 		game_test(ratio)
 
 
+
+
+
+
+
+
+
 def game_soubakan_integrationtest():
-	'''game()とsoubakan()の結合テスト
+	'''
+	## game_soubakan_integrationtest ver1.0
+	
+	__UPDATE1.0__
+	First commit
+	
+	__USAGE__
+	game_soubakan_integrationtest()のコメントアウトをはずす
+	
+	__INTRODUCTION__
+	game()とsoubakan()の結合テスト
+	
+	__ACTION__
 	soubakanは中央値1.4の正規分布に従う乱数返してくる
 	> ただし1<ratio<inf
 	gameはsoubakanから返ってきた勝率に従う確率でTrueを返す
 	Trueの返ってくる確率がsoubakanの中央値1.4の逆数に近づくはず
+
+	```run result
+	__________ ratio: 1.465780521846425 __________
+	Winrate : 0.682230 , Result : 0.678100
+	Subtraction 0.00413037835180996
+	```
+
+	* ratioは何倍のチケットを買ったのか
+	* Winrate:計算式から導かれる勝率(0で絶対勝てない、1で絶対勝てる)
+	* Result:関数gameに実際に10000回くらい勝ち負けを判定させ、帰ってきたTrueの回数を試行回数で割り算するとWinrateに近い値が出るはず
+	* Subtraction: WinratioとResultの差分
+	
+	__PLAN__
+	None
 	'''
 	low,high=1.01,np.inf
 	mu,si=1.4,0.3
 	x=soubakan(low, high, mu, si,1)
-	# for i in range(10):
 	game_test(float(x))
 
 
 
-'''__TEST__________________________'''
+'''__MAIN__________________________'''
 # soubakan__unitest()
 # game_unitest()
 game_soubakan_integrationtest()
