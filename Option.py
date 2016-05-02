73894146ee3dcec7011355b47580929c9b3aeeb3
u1and0 <e01.ando60@gmail.com>
2016-05-03 08:17:18 +0900

    commet modify, 初期資産を引数でコントロール

--- Option.py
+++ Option.py
@@ -1,35 +1,42 @@
-'''
-## Option ver1.0
-
-__UPDATE1.0__
-First commit
-
-__USAGE__
-?
-
-__INTRODUCTION__
-ratioから始まりprofitが決まるまでにClass使えるんじゃないか
-まだわからん
-
-__ACTION__
---action--
-
-__PLAN__
-None
-'''
 from game2 import *
 class Option:
-	"""docstring for Option"""
-	def __init__(self):
+	'''
+	## Option ver1.1
+
+	__UPDATE1.1__
+	引数に初期資産
+	printメッセージ修正
+
+	__UPDATE1.0__
+	First commit
+
+	__USAGE__
+	初期資産assetを引数に
+	結果を標準出力にprint
+
+	__INTRODUCTION__
+	プッシュ投資法をオプション取引でシミュレート
+
+	__ACTION__
+	init で初期値を決める
+	iterはinitの初期値を全てnextに渡す
+	nextでループ
+
+
+	__PLAN__
+	None
+	'''
+	def __init__(self,asset):
 		self.ratio=float(soubakan(low=1.01, high=np.inf, mu=1.4, si=0.3, length=1))
 		self.price = round(1000/self.ratio,-1)    #単価が決まる
 		self.unit = 1
 		self.bet=self.unit*self.price
 		self.last=self.bet
 		self.profit = self.unit*(1000-self.price)
-		self.asset=1000
+		self.asset=asset
+		print('--My asset is %d--'% self.asset)
 		print('--Game Start--')
-		print('ratio %f\nprice %d x unit %d=bet %d\npayout %d,profit %d\nasset will %d=>%d' %( self.ratio, self.price,self.unit,self.bet,self.unit*1000,self.profit,self.asset,self.profit+self.asset))
+		print('ratio %f\nprice %d x unit %d=bet %d\npayout %d,profit %d\nasset will be %d=>%d' %( self.ratio, self.price,self.unit,self.bet,self.unit*1000,self.profit,self.asset,self.profit+self.asset))
 		print('_'*20)
 
 
@@ -38,25 +45,23 @@ class Option:
 		return self
 
 	def __next__(self):
-		# while self.profit<self.bet*2:
 		self._i+=1
 		if self._i == 100:
 			raise StopIteration()
 		self.ratio=float(soubakan(low=1.01, high=np.inf, mu=1.4, si=0.3, length=1))
 		self.price = round(1000/self.ratio,-1)    #単価が決まる
-		# self.profit=0
-		# self.unit=0
 		self.last=self.bet
 		print('Before bet asset',self.asset)
 		print('bet -',self.last)
 		if self.asset<self.bet:
-			print('I have no more money!')
+			print('--I have no more money!--')
 			raise StopIteration()
 		self.asset-=self.bet
-		print('Before game asset',self.asset)
+		print('Before game asset %d'% self.asset)
 		import random
 		if random.random()>=1-1/self.ratio:
-			print('Win!+',self.unit*1000)
+			print('Win!')
+			print('asset +%d'% (self.unit*1000))
 			self.asset+=self.unit*1000
 			self.unit=1
 			self.bet=self.unit*self.price
@@ -67,7 +72,7 @@ class Option:
 				self.unit+=1
 				self.bet=self.unit*self.price
 				self.profit = self.unit*(1000-self.price)
-		print('After game asset',self.asset)
+		print('After game asset %d'% self.asset)
 		return self
 
 	# def price(self,ratio):
@@ -77,43 +82,10 @@ class Option:
 '''
 TEST
 '''
-x=Option()
+x=Option(10000)
 for i in x:
 	print('--Next Game--')
+
 	print('ratio %f\nprice %d x unit %d=bet %d\npayout %d,profit %d\nasset will %d=>%d' %( i.ratio, i.price,i.unit,i.bet,i.unit*1000,i.profit,i.asset,i.profit+i.asset))
+
 	print('_'*20)
-
-# j=1
-# asset=100000
-# while asset>0:
-# 	print('ratio%f\nprice%dxunit%d\n=bet%d\nprofit%d' %( x.ratio, x.price,x.unit,x.bet,x.profit))
-# 	asset-=x.bet
-# 	print('before:',asset)
-# 	asset+=x.profit
-# 	print('after:',asset)
-# 	print('_'*20)
-# 	j+=1
-# 	if j>100:break
-
-
-
-# print(x.rati\o)
-# print(x.next())
-# for i in x.ratio:
-# 	print(i)
-# for i in range(10):
-# 	y=next(i)
-# 	print(y)
-# 	j+=1
-# 	if j>100:break
-
-
-
-# from game2 import *
-# ratio=float(soubakan(low=1.01, high=np.inf, mu=1.4, si=0.3, length=1))
-# print(ratio)
-# price=round(1000/ratio,-1)
-# print(price)
-# unit=unit(bet=1000, price)
-# print(unit)
-
