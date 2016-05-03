a22bba68f5f5eed7c631387a36550e450c29e0d8
u1and0 <e01.ando60@gmail.com>
2016-05-04 00:08:36 +0900

    Option 1.3 plot safe default change

--- Option.py
+++ Option.py
@@ -1,7 +1,12 @@
 from game2 import *
 class Option:
 	'''
-	## Option ver1.2
+	## Option ver1.3
+
+	__UPDATE1.3__
+	plot機能
+	assetのsafeへの移動
+	初期資産の変更
 
 	__UPDATE1.2__
 	資産の推移をリストにappend
@@ -28,10 +33,7 @@ class Option:
 
 
 	__PLAN__
-	* 安全資産の移動
-	* 資産の推移をplot
-	* 初期資産を変化させる
-	* 最大掛け金評価モジュール maxbet
+	何度も試行する
 
 	'''
 	def __init__(self,defaultAsset):
@@ -41,7 +43,9 @@ class Option:
 		self.bet=self.ticket*self.price
 		self.last=self.bet
 		self.profit = self.ticket*(1000-self.price)
+		self.defaultAsset=defaultAsset
 		self.asset=defaultAsset
+		self.safe=0
 		print('--My asset is %d--'% self.asset)
 		print('--Game Start--')
 		print('ratio %f\nprice %d x ticket %d=bet %d\npayout %d,profit %d\nasset will be %d=>%d' %( self.ratio, self.price,self.ticket,self.bet,self.ticket*1000,self.profit,self.asset,self.profit+self.asset))
@@ -54,8 +58,6 @@ class Option:
 
 	def __next__(self):
 		self._i+=1
-		if self._i == 100:
-			raise StopIteration()
 		self.ratio=float(soubakan(low=1.01, high=np.inf, mu=1.4, si=0.3, length=1))
 		self.price = round(1000/self.ratio,-1)    #単価が決まる
 		self.last=self.bet
@@ -63,34 +65,61 @@ class Option:
 		print('bet -%d'% self.last)
 		if self.asset<self.bet:
 			print('--I have no more money!--')
-			raise StopIteration()
+			raise StopIteration()    #掛け金掛けられなければ終了
 		self.asset-=self.bet
 		print('Before game asset %d'% self.asset)
 		import random
-		if random.random()>=1-1/self.ratio:
+		if random.random()>=1-1/self.ratio:    #ゲームの勝敗を決める
 			print('Win!')
 			print('asset +%d'% (self.ticket*1000))
 			self.asset+=self.ticket*1000
+			if self.asset > self.defaultAsset*1.1:
+				self.safe+=self.defaultAsset*0.1
+				self.asset-=self.defaultAsset*0.1
+				print('# Move money +%d, safe money: %d'% (self.defaultAsset, self.safe))
 			self.ticket=1
 			self.bet=self.ticket*self.price
 			self.profit = self.ticket*(1000-self.price)
 		else:
 			print('Lose!')
-			while self.profit<self.last*2:
+			while self.profit<self.last*2:    #次の賭けチケット数計算
 				self.ticket+=1
 				self.bet=self.ticket*self.price
 				self.profit = self.ticket*(1000-self.price)
+			import maxbet
+			self.ticket=min(maxbet.maxbet(self.asset),self.ticket)    #最大掛け金評価
 		print('After game asset %d'% self.asset)
 		return self
+
+
+
+
+import matplotlib.pyplot as plt
+def plotter(s,l):
+	'''収益曲線(profitList)と資産曲線(assetList)を描画する'''
+	xaxis=range(len(l))
+	lbl='Default Asset '+str(s)
+	plt.plot(range(len(l)),l,label=lbl)
+	plt.legend(loc='best',fancybox=True,fontsize='small')
+	plt.xlabel('Game streak')
+	plt.grid(True)
+
+
+
 
 
 '''
 TEST
 '''
-x=Option(10000)
-asset=[]
-for i in x:
-	print('--%d Game--\nratio %f\nprice %d x ticket %d=bet %d\npayout %d,profit %d\nasset will %d=>%d' %(i._i, i.ratio, i.price,i.ticket,i.bet,i.ticket*1000,i.profit,i.asset,i.profit+i.asset))
-	print('_'*20)
-	asset.append(i.asset)
-print(asset)
+assetList=[]
+profitList=[]
+for y in [1000000]:
+	x=Option(y)
+	for i in x:
+		print('--%d Game--\nratio %f\nprice %d x ticket %d=bet %d\npayout %d,profit %d\nasset will %d=>%d' %(i._i, i.ratio, i.price,i.ticket,i.bet,i.ticket*1000,i.profit,i.asset,i.profit+i.asset))
+		print('_'*20)
+		# assetList.append(i.asset)
+		profitList.append(i.asset-y)
+	print(profitList)
+	plotter(y,profitList)
+plt.show()
